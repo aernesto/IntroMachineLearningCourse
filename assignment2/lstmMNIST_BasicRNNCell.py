923838d7f461870a54ac34237102da1644712a56
adrian <adrian.radillo@gmail.com>
2017-10-24 21:10:35 -0500

    first RNN run - question 3a

---
+++ assignment2/lstmMNIST_BasicRNNCell.py
@@ -0,0 +1,85 @@
+# Most code inspired from
+# https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
+import tensorflow as tf
+from tensorflow.python.ops import rnn, rnn_cell, rnn_cell_impl
+#from tensorflow.contrib import rnn  # copied from Damien Aymeric
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+# call mnist function
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+learningRate = 1e-4
+trainingIters = 10000
+batchSize = 100
+displayStep = 20
+
+nInput = 28  # we want the input to take the 28 pixels
+nSteps = 28  # every 28
+nHidden = 128  # number of neurons for the RNN
+nClasses = 10  # this is MNIST so you know
+
+x = tf.placeholder('float', [None, nSteps, nInput])
+y = tf.placeholder('float', [None, nClasses])
+
+weights = {
+    'out': tf.Variable(tf.random_normal([nHidden, nClasses]))
+}
+
+biases = {
+    'out': tf.Variable(tf.random_normal([nClasses]))
+}
+
+
+def RNN(x, weights, biases):
+    # current code from Damien Aymeric
+    x = tf.unstack(x, nSteps, 1)
+
+    # Define a lstm cell with tensorflow
+    lstm_cell = rnn_cell.BasicRNNCell(nHidden)
+
+    # Get lstm cell output
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+
+    # Linear activation, using rnn inner loop last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+pred = RNN(x, weights, biases)
+
+# optimization
+# create the cost, optimization, evaluation, and accuracy
+# for the cost softmax_cross_entropy_with_logits seems really good
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred))
+optimizer = tf.train.AdamOptimizer(learning_rate=learningRate).minimize(cost)
+
+correctPred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+
+    while (step * batchSize) < trainingIters:
+        # print('hi, this is iteration ', step, 'of the while loop')
+        batchX, batchY = mnist.train.next_batch(batchSize)  # mnist has a way to get the next batch
+        batchX = batchX.reshape((batchSize, nSteps, nInput))
+
+        sess.run(optimizer, feed_dict={x: batchX, y: batchY})
+
+        if step % displayStep == 0:
+            acc = sess.run(accuracy, feed_dict={x: batchX, y: batchY})
+            loss = sess.run(cost, feed_dict={x: batchX, y: batchY})
+            print("Iter " + str(step * batchSize) +
+                  ", Minibatch Loss= " +
+                  "{:.6f}".format(loss) + ", Training Accuracy= " +
+                  "{:.5f}".format(acc))
+        step += 1
+    print('Optimization finished')
+
+    testData = mnist.test.images.reshape((-1, nSteps, nInput))
+    testLabel = mnist.test.labels
+    print("Testing Accuracy:",
+          sess.run(accuracy, feed_dict={x: testData, y: testLabel}))
